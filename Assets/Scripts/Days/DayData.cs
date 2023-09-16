017596d2b91bdd8aa568cd656b8a83a7690e9c2a
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2023-09-16 16:01:56 +0200

    Menu and school background sprites, Delete input system samples

---
+++ Assets/Scripts/Days/DayData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DayData : ScriptableObject
+{
+    public List<string> miniGames;
+
+    public bool isCutscene = false;
+
+    public string dialogue = "";
+
+    public int numberOfMeds = -1;
+
+    public float time;
+    public float timeBalance = -1f;
+}
