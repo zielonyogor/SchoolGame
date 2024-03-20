389133002e4fecf734a1e9dbff6fd755c2f142aa
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2024-03-20 17:03:41 +0100

    Table change size

---
+++ Assets/Scripts/Minigames/ChangeSize.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ChangeSize : MonoBehaviour
+{
+    // Reference to the SpriteRenderer component
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider;
+
+    [SerializeField] int tableSize = 100;
+
+    private void Start()
+    {
+        tableSize = MiniGameManager.instance.tableSize;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
+
+        spriteRenderer.size = new Vector2(tableSize,15f);
+        boxCollider.size = spriteRenderer.bounds.size;
+    }
+}
