16c44023c0ee37f16ec912c016b678fc4cc526fb
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2024-03-27 10:26:06 +0100

    Credits Animation, improved Meds Randomizer

--- Assets/Scripts/Minigames/DraggableManager.cs
+++ Assets/Scripts/Minigames/DraggableManager.cs
@@ -25,36 +25,24 @@ public class DraggableManager : MonoBehaviour
             child.gameObject.tag = "DraggableIncorrect";
         }
 
-        //Try this later
-        //List<Transform> tempList = new List<Transform>(transforms);
-
-        //// Fisher-Yates shuffle
-        //for (int i = tempList.Count - 1; i > 0; i--)
-        //{
-        //    int randomIndex = Random.Range(0, i + 1);
-        //    Transform temp = tempList[i];
-        //    tempList[i] = tempList[randomIndex];
-        //    tempList[randomIndex] = temp;
-        //}
-
-        //// Select the first 'count' elements
-        //Transform[] randomTransforms = new Transform[count];
-        //for (int i = 0; i < count; i++)
-        //{
-        //    randomTransforms[i] = tempList[i];
-        //}
+        // Fisher-Yates shuffle
+        for (int i = items.Count - 1; i > 0; i--)
+        {
+            int randomIndex = Random.Range(0, i + 1);
+            Transform temp = items[i];
+            items[i] = items[randomIndex];
+            items[randomIndex] = temp;
+        }
 
         for (int i = 0; i < numberOfMeds; i++)
         {
-            int randomIndex = i; //Random.Range(0, items.Count - i); //this weird
-            pickedChildren.Add(items[randomIndex]);
-            items[randomIndex].gameObject.tag = "DraggableCorrect";
-            //Do I need to rotate this?? Maybe a little bit, Should Move around also
-            items[randomIndex].transform.rotation = Quaternion.Euler(0, 0, Random.Range(-80,80));
+            pickedChildren.Add(items[i]);
+            items[i].gameObject.tag = "DraggableCorrect";
+            items[i].transform.position = new Vector2(Random.Range(-40, 30), Random.Range(-48, 16));
 
-            images[i].sprite = items[randomIndex].GetComponent<SpriteRenderer>().sprite;
-            items.RemoveAt(randomIndex);
+            images[i].sprite = items[i].GetComponent<SpriteRenderer>().sprite;
         }
+
     }
 
 }
