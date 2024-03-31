678cbd5d1882e919b2b8b1a67f77b6fbe423ac27
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2024-03-31 23:38:39 +0200

    Randomize position of puzzles without colliding

--- Assets/Scripts/Minigames/PuzzleManager.cs
+++ Assets/Scripts/Minigames/PuzzleManager.cs
@@ -5,27 +5,53 @@ using UnityEngine;
 public class PuzzleManager : MonoBehaviour
 {
     private int numberOfPuzzles;
+    public List<Transform> objects;
 
     void Start()
     {
         numberOfPuzzles = MiniGameManager.instance.numberOfPuzzles;
+        objects = new List<Transform>();
         for(int i = 0; i < numberOfPuzzles; i++)
         {
-            ChangePosition(transform.GetChild(i));
-            ChangePosition(transform.GetChild(i).GetChild(0));
+            objects.Add(transform.GetChild(i));
+            ChangePosition(i*2);
+            objects.Add(transform.GetChild(i).GetChild(0));
+            ChangePosition(i*2+1);
+
             transform.GetChild(i).gameObject.SetActive(true);
         }
     }
 
-    void ChangePosition(Transform t)
+    void ChangePosition(int index)
     {
         int randomX = Random.Range(-80, 80);
         int randomY = Random.Range(-32, 48);
-        while (Mathf.Abs(randomX) <= 46 && randomY <= 4)
+        int i = 0;
+        while (true)
         {
+            objects[index].position = new Vector2(randomX, randomY);
+            if (!(Mathf.Abs(randomX) <= 46 && randomY <= 5) && !isColliding(index))
+            {
+                break;
+            }
             randomX = Random.Range(-80, 80);
             randomY = Random.Range(-32, 48);
+            i++;
+            if (i >= 100)
+                break;
         }
-        t.position = new Vector2(randomX, randomY);
+    }
+
+    private bool isColliding(int index)
+    {
+        for (int i = 0; i < index; i++)
+        {
+            float distance = Vector2.Distance(objects[index].position, objects[i].position);
+            if (distance < 14f) //11 or sth was enough but with 14 it's better (holes aren't on each other)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
