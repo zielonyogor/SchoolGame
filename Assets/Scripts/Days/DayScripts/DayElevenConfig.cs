48766a494b808c671f4228dff902b1c3b13bedc4
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2024-07-15 00:05:32 +0200

    New SlidingMap layouts

--- Assets/Scripts/Days/DayScripts/DayElevenConfig.cs
+++ Assets/Scripts/Days/DayScripts/DayElevenConfig.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Day_11", menuName = "DayConfigs/Day_11")]
 public class DayElevenConfig : Levelnfo
 {
-    public SlidingMapLayout slidingMapLayout;
+    public int numberOfPuzzles;
     public string dialogueText;
 
     public override void LoadData()
     {
         base.LoadData();
-        MiniGameManager.instance.slidingMapLayout = slidingMapLayout;
+        MiniGameManager.instance.numberOfPuzzles = numberOfPuzzles;
         MiniGameManager.instance.dialogueText = dialogueText;
     }
 }
